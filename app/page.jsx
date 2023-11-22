294c336b3c5a66be9558e8ea8d6d5d8cefb84612
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-11-22 11:31:32 -0300

    Estilizado detalhes do meditrack

--- app/page.jsx
+++ app/page.jsx
@@ -23,20 +23,20 @@ export default function Home() {
         </h2>
         <h3 className="text-3xl mb-10">O App que pensa nas suas consultas por você</h3>
         <div>
-          <div className="flex justify-around">
-            <div className="w-1/3 mt-14">
+          <div className="flex justify-center">
+            <div className="w-1/3 mt-14 mr-20">
               <h4 className="text-2xl m-2 text-[#1EF409]">Gestão de consultas</h4>
               <p>
                 Lembra pacientes e médicos de maneira integrada sobre consultas
                 futuras, garantindo interações de saúde pontuais.
               </p>
             </div>
-            <Image src="/img/desc1.jpg" width={500} height={350} />
+            <Image className="w-1/3" src="/img/desc1.jpg" width={500} height={350} />
           </div>
-          <div className="flex justify-around">
-            <Image src="/img/desc2.jpg" width={500} height={350} />
-            <div>
-              <h4>Lembretes detalhados</h4>
+          <div className="flex justify-center mt-7 mb-7">
+            <Image className="mr-20 w-1/3" src="/img/desc2.jpg" width={500} height={350} />
+            <div className="w-1/3 mt-14">
+              <h4 className="text-2xl m-2 text-[#1EF409]">Lembretes detalhados</h4>
               <p>
                 Oferece informações detalhadas sobre consultas, incluindo data,
                 horário, médico e endereço do hospital, com um link conveniente
@@ -44,16 +44,16 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex justify-around">
-            <div>
-              <h4>Triagem de Sintomas</h4>
+          <div className="flex justify-center">
+            <div className="w-1/3 mt-14 mr-20">
+              <h4 className="text-2xl m-2 text-[#1EF409]">Triagem de Sintomas</h4>
               <p>
                 Possibilita pacientes a avaliarem seus sintomas com través do
                 uso de inteligência artificial e os direciona para o hospital
                 mais próximo para atendimento médico rápido.
               </p>
             </div>
-            <Image src="/img/desc3.jpg" width={500} height={350} />
+            <Image className="w-1/3" src="/img/desc3.jpg" width={500} height={350} />
           </div>
         </div>
       </section>

abbe6979fb4b5f1ccf53c85ca0d8e31ae1b0ad0e
gutolive09 <gutolive01@gmail.com>
2023-11-23 23:33:16 -0300

    Estilização de formCadastro finalizada.

--- components/FormCadastro/index.jsx
+++ components/FormCadastro/index.jsx
@@ -6,36 +6,36 @@ export default function FormCadastro() {
       <fieldset className="flex flex-col justify-centera items-center gap-8">
         <legend className=" text-white text-center text-3xl mb-5">Cadastre-se</legend>
         <div className="flex flex-col items-center w-full gap-2">
-          <label className="text-white text-xl" htmlFor="">Nome completo</label>
-          <input type="text" className="w-1/2 rounded-xl px-2"/>
+          <label className="text-white text-xl" htmlFor="idNome">Nome completo</label>
+          <input type="text" name="nome" id="idNome" className="w-1/2 rounded-xl px-2"/>
         </div>
         <div className="flex flex-col items-center w-full gap-2">
-          <label className="text-white text-xl" htmlFor="">Email</label>
-          <input type="text" className="w-1/2 rounded-xl px-2"/>
+          <label className="text-white text-xl" htmlFor="idEmail">Email</label>
+          <input type="text" name="email" id="idEmail" className="w-1/2 rounded-xl px-2"/>
         </div>
         <div className="flex flex-col items-center w-full gap-2">
-          <label className="text-white text-xl" htmlFor="">Cpf</label>
-          <input type="text" className="w-1/2 rounded-xl px-2"/>
+          <label className="text-white text-xl" htmlFor="idCpf">Cpf</label>
+          <input type="text" name="cpf" id="idCpf" className="w-1/2 rounded-xl px-2"/>
         </div>
         <div className="flex flex-col items-center w-full gap-2">
-          <label className="text-white text-xl" htmlFor="">Senha</label>
-          <input type="text"className="w-1/2 rounded-xl px-2" />
+          <label className="text-white text-xl" htmlFor="idSenha">Senha</label>
+          <input type="password" name="senha" id="idSenha" className="w-1/2 rounded-xl px-2" />
         </div>
-        <div>
+        <div className="text-white">
           <p>A sua senha deve ter:</p>
-          <ul>
-            <li>8 caracteres ou mais</li>
+          <ul className="list-disc pl-8">
+            <li >8 caracteres ou mais</li>
             <li>Letras maiúsculas e minúsculas</li>
             <li>Pelo menos um caractere especial(@,!,#...)</li>
           </ul>
         </div>
         <div className="flex flex-col items-center w-full gap-2">
-          <label className="text-white text-xl" htmlFor="">Confirme sua senha</label>
-          <input type="text" className="w-1/2 rounded-xl px-2"/>
+          <label className="text-white text-xl" htmlFor="idSenhaConf">Confirme sua senha</label>
+          <input type="password" name="senhaConf" id="idSenhaConf" className="w-1/2 rounded-xl px-2"/>
         </div>
-        <div className="flex flex-col items-center w-full gap-2">
-          <input className="text-white text-xl" type="checkbox" name="" id="" />
-          <label htmlFor="">
+        <div>
+          <input type="checkbox" name="termos" id="idTermos" />
+          <label className="text-white px-3 " htmlFor="idTermos">
             Você leu e concorda com os termos de privacidade
           </label>
         </div>

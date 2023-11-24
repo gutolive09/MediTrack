7ca8965dcaaab9e404a070eea0d97be4173d45d5
gutolive09 <gutolive01@gmail.com>
2023-11-24 14:04:11 -0300

    Primeiros membros adicionados ao sobrenos.

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -2,47 +2,47 @@ import Image from "next/image";
 
 export default function SobreNos() {
   return (
-    <section className="mx-auto w-1/2">
-        <div className=" bg-[--verde-secundario] flex gap-28">
-            <Image width={380} height={380}/>
-            <div>
-                <h2>André Rohregger</h2>
-                <p>Estagiário</p>
-                <Image src="/img/Estagiario.png" width={200} height={200}/>
-            </div>
+    <section className="mx-auto w-1/2 flex flex-col gap-10">
+      <div className=" bg-[--verde-secundario] flex gap-28 h-[380px] p-4">
+        <Image className="object-cover rounded-3xl" src="/img/Andre.jpg" width={380} height={380} />
+        <div className="text-white w-full flex flex-col items-center gap-8">
+          <h2 className="text-4xl">André Rohregger</h2>
+          <p className="text-2xl">Estagiário</p>
+          <Image src="/img/Estagiario.png" width={200} height={200} />
         </div>
-        <div className="flex">
-            <Image width={380} height={380}/>
-            <div>
-                <h2>Gabriel Siqueira</h2>
-                <p>DEV Python Sênior</p>
-                <Image src="/img/Python.png" width={200} height={200}/>
-            </div>
+      </div>
+      <div className="flex h-[380px]">
+        <Image width={380} height={380} />
+        <div className="text-white w-full flex flex-col items-center gap-8">
+          <h2 className="text-4xl">Gabriel Siqueira</h2>
+          <p className="text-2xl">DEV Python Sênior</p>
+          <Image src="/img/Python.png" width={200} height={200} />
         </div>
-        <div className=" bg-[--verde-secundario] flex">
-            <Image width={380} height={380}/>
-            <div>
-                <h2>Gustavo de Oliveira Azevedo</h2>
-                <p>Lead Front-End</p>
-                <Image src="/img/Ui-UX.png" width={200} height={200}/>
-            </div>
+      </div>
+      <div className=" bg-[--verde-secundario] flex h-[380px]">
+        <Image width={380} height={380} />
+        <div className="text-white w-full flex flex-col items-center gap-8">
+          <h2 className="text-4xl">Gustavo de Oliveira Azevedo</h2>
+          <p className="text-2xl">Lead Front-End</p>
+          <Image src="/img/Ui-UX.png" width={200} height={200} />
         </div>
-        <div className="flex">
-            <Image width={380} height={380}/>
-            <div>
-                <h2>Mateus Mantovani Araújo</h2>
-                <p>DEV Java Sênior</p>
-                <Image src="/img/Java.png" width={200} height={200}/>
-            </div>
+      </div>
+      <div className="flex h-[380px]">
+        <Image className="object-cover rounded-3xl" src="/img/Mateus.jpg" width={380} height={380} />
+        <div className="text-white w-full flex flex-col items-center gap-8">
+          <h2 className="text-4xl">Mateus Mantovani Araújo</h2>
+          <p className="text-2xl">DEV Java Sênior</p>
+          <Image src="/img/Java.png" width={200} height={200} />
         </div>
-        <div className=" bg-[--verde-secundario] flex">
-            <Image width={380} height={380}/>
-            <div>
-                <h2>Juan de Godoy</h2>
-                <p>Analista Sênior</p>
-                <Image src="/img/DataBase.png" width={200} height={200}/>
-            </div>
+      </div>
+      <div className=" bg-[--verde-secundario] flex  h-[380px]">
+        <Image width={380} height={380} />
+        <div className="text-white w-full flex flex-col items-center gap-8">
+          <h2 className="text-4xl">Juan de Godoy</h2>
+          <p className="text-2xl">Analista Sênior</p>
+          <Image src="/img/DataBase.png" width={200} height={200} />
         </div>
+      </div>
     </section>
-  )
+  );
 }
